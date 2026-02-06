ec554eb0f40d10823dc9a29f0888a1af50a51c1f
Tom Walker <mrthomasnwalker@outlook.com>
2026-02-06 20:15:32 +0000

    Tidying up

--- mainwindow.py
+++ mainwindow.py
@@ -10,20 +10,16 @@ from PySide6.QtWidgets import (QApplication, QCheckBox, QTreeWidget,
 )
 
 class MainWindow(QMainWindow):
+    """GUI Macro Tester main window class"""
+
     def __init__(self, parent=None):
+        """Constructor"""
+
         super().__init__(parent)
         self.setWindowTitle("GUI Macro Tester")
-        # label = QLabel(str(screenWidth) + " x " + str(screenHeight))
-
         self.setFixedSize(500, 800)
 
-        ''''
-        table = QTableWidget()
-        table.setRowCount(20)
-        table.setColumnCount(3)
-        table.setHorizontalHeaderLabels(["Screen", "Area Name", "Position"])
-        '''
-
+        # Screen co-ordinates tree
         self.tree = QTreeWidget(self)
         self.tree.setColumnCount(2)
         self.tree.setColumnWidth(0, 350)
@@ -31,17 +27,17 @@ class MainWindow(QMainWindow):
         treedata = load_default()
         self.populateTree(treedata)
 
-        # The `Qt` namespace has a lot of attributes to customize
-        # widgets. See: http://doc.qt.io/qt-6/qt.html
-        # label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
         self.setCentralWidget(self.tree)
 
         self.createMenus()
 
+
     def populateTree(self, data):
+        """
+        Populate tree view from JSON data
+        
+        :param data: Dict generated from JSON file
+        """
         screens = []
         for k, v in data.items():
             screen = QTreeWidgetItem([k])
@@ -51,24 +47,11 @@ class MainWindow(QMainWindow):
                     name = QTreeWidgetItem([areaName, pos])
                     screen.addChild(name)
             screens.append(screen)
-
-        '''
-        items = []
-        for key, values in data.items():
-            item = QTreeWidgetItem([key])
-            for value in values:
-                print(value)
-                print(type(value))
-                ext = value.values()
-                child = QTreeWidgetItem([value, ext])
-                item.addChild(child)
-            items.append(item)
-
-        tree.insertTopLevelItems(0, items)
-        '''
         self.tree.insertTopLevelItems(0, screens)
 
+
     def createMenus(self):
+        """Create menus and toolbars"""
 
         # Toolbar
         toolbar = QToolBar("My main toolbar")
@@ -108,20 +91,30 @@ class MainWindow(QMainWindow):
         file_submenu = file_menu.addMenu("Submenu")
         file_submenu.addAction(button_action2)
 
+
     def toolbar_button_clicked(self, s):
+        """Debug function for button click"""
+
         print("click", s)
 
+
     def go_button_clicked(self):
+        """
+        Execute current macro sequence
+        """
+        
         program_name, ok = QInputDialog.getText(self, 'Enter progam name', 'Name of new program:')
         if ok and program_name:
             new_program(program_name)
 
+    
     def detect_position(self):
+        """Debug function to show main window co-ordinates"""
+
         print(getattr(self, "pos")())
 
 
-
-
+# Run the application
 if __name__ == "__main__":
     app = QApplication([])
     window = MainWindow()
--- util.py
+++ util.py
@@ -1,5 +1,12 @@
 import json
 
+# The `Qt` namespace has a lot of attributes to customize
+# widgets. See: http://doc.qt.io/qt-6/qt.html
+# label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+# Set the central widget of the Window. Widget will expand
+# to take up all the space in the window by default.
+
 def load_default():
     with open('default.json') as json_file:
         return json.load(json_file)
